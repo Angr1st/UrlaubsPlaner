39e27a7b9e956683dabff4ebee3e879cf98f0cbe
Torben Clasen <torbenclasen@gmail.com>
2019-08-20 02:40:10 +0200

    WIP

---
+++ UrlaubsPlaner.FSharp/DBInteraction/DBInteraction.fs
@@ -0,0 +1,9 @@
+﻿namespace UrlaubsPlaner.FSharp
+
+open FSharp.Data
+
+module DBInteraction =
+    [<Literal>]
+    let ConnectionString = "Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=UrlaubsplanerDB;Integrated Security=True;"
+
+    type Sql = SqlCommandProvider<,ConnectionString
