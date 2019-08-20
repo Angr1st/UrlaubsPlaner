878c2e9afcac32d543e7e42cb17ec7051d9113b5
Torben Clasen <torbenclasen@gmail.com>
2019-08-20 23:47:46 +0200

    Added the last two Logic Modules

---
+++ UrlaubsPlaner.FSharp/Controllers/AbsenceType_FormLogic.fs
@@ -0,0 +1,7 @@
+﻿namespace UrlaubsPlaner.FSharp
+
+open UrlaubsPlanerForms
+
+module AbsenceType_FormLogic =
+
+    let AbsenceTypeForm = new AbsenceType_Form()
---
+++ UrlaubsPlaner.FSharp/Controllers/Employee_FormLogic.fs
@@ -0,0 +1,7 @@
+﻿namespace UrlaubsPlaner.FSharp
+
+open UrlaubsPlanerForms
+
+module Employee_FormLogic =
+
+    let EmployeeForm = new Employee_Form()
