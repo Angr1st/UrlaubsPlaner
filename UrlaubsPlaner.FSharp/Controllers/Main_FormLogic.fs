5413a58c5982eb8ff3708d2c8c1f676ec3c49e8f
Torben Clasen <torbenclasen@gmail.com>
2019-08-23 23:04:10 +0200

    Added First Event

--- UrlaubsPlaner.FSharp/Controllers/Main_FormLogic.fs
+++ UrlaubsPlaner.FSharp/Controllers/Main_FormLogic.fs
@@ -5,3 +5,8 @@ open UrlaubsPlanerForms
 module Main_FormLogic =
 
     let MainForm = new Main_Form()
+
+
+
+    let registerEvents (form:Main_Form) =
+        form.Load.Add(fun evenArgs -> System.Console.Beep())
