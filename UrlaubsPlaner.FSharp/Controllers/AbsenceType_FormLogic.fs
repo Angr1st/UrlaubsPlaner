edc39b48afb4a589a577dbc1a24d96d7b3428f61
Torben Clasen <torbenclasen@gmail.com>
2019-08-25 22:33:44 +0200

    WIP on the Event registration

--- UrlaubsPlaner.FSharp/Controllers/AbsenceType_FormLogic.fs
+++ UrlaubsPlaner.FSharp/Controllers/AbsenceType_FormLogic.fs
@@ -4,4 +4,37 @@ open UrlaubsPlanerForms
 
 module AbsenceType_FormLogic =
 
+    type DataStorage_AbsenceType = 
+        {
+            AbsenceTypes:DB.Querys.GetAbsenceTypes.Record array
+        }
+    
+    let mutable private IsInsert = true
+
+    let mutable private Data = {AbsenceTypes=Array.empty}
+
     let AbsenceTypeForm = new AbsenceType_Form()
+
+    let UpdateData (form:AbsenceType_Form) =
+        do form.absenceTypeListView.Items.Clear()
+
+        let absenceTypes = DB.Querys.getAbsenceTypes DB.DBInteraction.ConnectionString
+
+        Data <- {AbsenceTypes= absenceTypes.Execute() |> Seq.toArray}
+
+        let transformAbsenceTypeToListViewItem (absenceTypeRecord:DB.Querys.GetAbsenceTypes.Record) =
+            System.Windows.Forms.ListViewItem(
+                [|
+                    absenceTypeRecord.AbsenceTypeID.ToString()
+                    absenceTypeRecord.Label
+                |]
+            )
+
+        let listViewAbsenceTypeArray = Data.AbsenceTypes |> Array.map transformAbsenceTypeToListViewItem
+
+        form.absenceTypeListView.Items.AddRange listViewAbsenceTypeArray
+
+    let registerEvents (form:AbsenceType_Form) =
+        form.cancelBtn.Click.Add(fun evenArgs -> form.Hide())
+
+        form.Load.Add(fun evenArgs -> UpdateData form)
