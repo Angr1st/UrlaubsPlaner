da2a9ba042d257bb38573d7330d483ec49f859fd
Torben Clasen <torbenclasen@gmail.com>
2019-08-26 00:55:12 +0200

    Added the call to register the events on the Employee Form

--- UrlaubsPlaner.FSharp/Program.fs
+++ UrlaubsPlaner.FSharp/Program.fs
@@ -12,6 +12,7 @@ module Main =
         
         Main_FormLogic.registerEvents Main_FormLogic.MainForm
         AbsenceType_FormLogic.registerEvents AbsenceType_FormLogic.AbsenceTypeForm
-        
+        Employee_FormLogic.registerEvents Employee_FormLogic.EmployeeForm
+
         Application.Run(Main_FormLogic.MainForm)
         0 // return an integer exit code
